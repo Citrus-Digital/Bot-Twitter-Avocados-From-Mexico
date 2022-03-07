79b2b2e4e9f0d81a5535a848da74d008ec03e533
Raphael Monteiro <raphaelmonteiro@Raphaels-MBP-6.lan>
2022-03-07 22:13:43 +0000

    remove trigger #ColorectalCancerAwarenessMonth and remove hashtag #FxckCancer

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -9,35 +9,35 @@ module.exports = [
                 responses: [
                     {
                         id: 104, 
-                        response: "Congratulations on accepting this mission! 🚀 Your first command will be to go deep into the unknown 🪐. Learn more about your mission, visit https://bit.ly/3p7OoNU  #MissionToUranus  #fxckcancer #CheckYourColon"
+                        response: "Congratulations on accepting this mission! 🚀 Your first command will be to go deep into the unknown 🪐. Learn more about your mission, visit https://bit.ly/3p7OoNU  #MissionToUranu  #CheckYourColon"
                     },
                     {
                         id: 105, 
-                        response: "The countdown to launch this mission started. 🚀 The objective? To save mankind. Become a hero 👨‍🚀 👩‍🚀. Get a colonoscopy. Learn more about it here https://bit.ly/3p7OoNU  #MissionToUranus  #fxckcancer #CheckYourColon"
+                        response: "The countdown to launch this mission started. 🚀 The objective? To save mankind. Become a hero 👨‍🚀 👩‍🚀. Get a colonoscopy. Learn more about it here https://bit.ly/3p7OoNU  #MissionToUranus #CheckYourColon"
                     },
                     {
                         id: 106, 
-                        response: "Thanks for joining the mission, Captain. Uranus might have the answer to a longer life on 🌎. Shuttle up and get ready for a journey 🚀that will change your life forever.Visit https://bit.ly/3p7OoNU  #MissionToUranus  #fxckcancer #CheckYourColon"
+                        response: "Thanks for joining the mission, Captain. Uranus might have the answer to a longer life on 🌎. Shuttle up and get ready for a journey 🚀that will change your life forever.Visit https://bit.ly/3p7OoNU  #MissionToUranus #CheckYourColon"
                     },
                     {
                         id: 107, 
-                        response: "U r a hero in the making! The first step towards longer life on 🌎starts with Uranus. This mission’s on you. Get ready for it. Earn a journey for a lifetime 🚀. Visit https://bit.ly/3p7OoNU  #MissionToUranus  #fxckcancer #CheckYourColon"
+                        response: "U r a hero in the making! The first step towards longer life on 🌎starts with Uranus. This mission’s on you. Get ready for it. Earn a journey for a lifetime 🚀. Visit https://bit.ly/3p7OoNU  #MissionToUranus #CheckYourColon"
                     },
                     {
                         id: 108, 
-                        response: "Good job, Captain! Here is the mission to extend life on 🌎. The exploration requires bravery. 👨‍🚀 👩‍🚀 & civilians are welcome to apply. Start the mission with a colonoscopy, visit https://bit.ly/3p7OoNU  #MissionToUranus #fxckcancer #CheckYourColon"
+                        response: "Good job, Captain! Here is the mission to extend life on 🌎. The exploration requires bravery. 👨‍🚀 👩‍🚀 & civilians are welcome to apply. Start the mission with a colonoscopy, visit https://bit.ly/3p7OoNU  #MissionToUranus #CheckYourColon"
                     },
                     {
                         id: 109, 
-                        response: "Welcome to the mission. After so much research up in space 🌌, finally, Uranus has the answer for how to save mankind. Learn more, visit https://bit.ly/3p7OoNU  #MissionToUranus #fxckcancer #CheckYourColon"
+                        response: "Welcome to the mission. After so much research up in space 🌌, finally, Uranus has the answer for how to save mankind. Learn more, visit https://bit.ly/3p7OoNU  #MissionToUranus #CheckYourColon"
                     },
                     {
                         id: 110, 
-                        response: "Welcome aboard. This mission is just one small step for you, one giant leap for your loved ones. Get a colonoscopy. Visit to learn more https://bit.ly/3p7OoNU  #MissionToUranus #fxckcancer #CheckYourColon"
+                        response: "Welcome aboard. This mission is just one small step for you, one giant leap for your loved ones. Get a colonoscopy. Visit to learn more https://bit.ly/3p7OoNU  #MissionToUranus #CheckYourColon"
                     },
                     {
                         id: 111, 
-                        response: "Uranus is the most important mission on Earth. Did you know that 9 in 10 people beat colon cancer when caught early with a colonoscopy? Visit and learn more https://bit.ly/3p7OoNU  #MissionToUranus #fxckcancer #CheckYourColon"
+                        response: "Uranus is the most important mission on Earth. Did you know that 9 in 10 people beat colon cancer when caught early with a colonoscopy? Visit and learn more https://bit.ly/3p7OoNU  #MissionToUranus #CheckYourColon"
                     },
                 ]
             }
@@ -53,23 +53,23 @@ module.exports = [
                 responses: [
                     {
                         id: 112, 
-                        response: "This is your mission! Extract unknown material from the deepest craters in the universe with a colonoscopy. If you succeed, you may win more experiences on planet 🌎. https://bit.ly/3p7OoNU  #MissionToUranus #fxckcancer #CheckYourColon"
+                        response: "This is your mission! Extract unknown material from the deepest craters in the universe with a colonoscopy. If you succeed, you may win more experiences on planet 🌎. https://bit.ly/3p7OoNU  #MissionToUranus #CheckYourColon"
                     },
                     {
                         id: 113, 
-                        response: "We hear you are joining the mission and are ready to explore 🚀 Uranus. Save mankind, including your own life. Get a colonoscopy. Learn more about your mission, visit https://bit.ly/3p7OoNU  #MissionToUranus #fxckcancer #CheckYourColon"
+                        response: "We hear you are joining the mission and are ready to explore 🚀 Uranus. Save mankind, including your own life. Get a colonoscopy. Learn more about your mission, visit https://bit.ly/3p7OoNU  #MissionToUranus #CheckYourColon"
                     },
                     {
                         id: 114, 
-                        response: "We 👀you are interested in our mission to save mankind. Great! Let’s start on Uranus. Get a colonoscopy. Visit to learn more https://bit.ly/3p7OoNU  #MissionToUranus  #fxckcancer #CheckYourColon"
+                        response: "We 👀you are interested in our mission to save mankind. Great! Let’s start on Uranus. Get a colonoscopy. Visit to learn more https://bit.ly/3p7OoNU  #MissionToUranus #CheckYourColon"
                     },
                     {
                         id: 115, 
-                        response: "Your mission starts on Uranus. It is said it has all the answers to extend mankind’s life on planet 🌎. One colonoscopy at a time. Visit https://bit.ly/3p7OoNU  #MissionToUranus  #fxckcancer #CheckYourColon"
+                        response: "Your mission starts on Uranus. It is said it has all the answers to extend mankind’s life on planet 🌎. One colonoscopy at a time. Visit https://bit.ly/3p7OoNU  #MissionToUranus #CheckYourColon"
                     },
                     {
                         id: 116, 
-                        response: "You R right, no need to be a space enthusiast to explore Uranus. This is your mission to accomplish a healthy colon. Learn more, visit https://bit.ly/3p7OoNU  #MissionToUranus #fxckcancer #CheckYourColon"
+                        response: "You R right, no need to be a space enthusiast to explore Uranus. This is your mission to accomplish a healthy colon. Learn more, visit https://bit.ly/3p7OoNU  #MissionToUranus #CheckYourColon"
                     }
                 ]
             }
@@ -77,7 +77,7 @@ module.exports = [
     },
     { 
         id: 113,
-        trigger: '#Fxckcancer',
+        trigger: ',
         responses: [
             {
                 start: '2022-01-24 00:00:00',
@@ -85,31 +85,15 @@ module.exports = [
                 responses: [
                     {
                         id: 117, 
-                        response: "RT @NAME We agree, #fxckcancer. Join our #missiontouranus to help save mankind. Visit https://bit.ly/3p7OoNU"
+                        response: "RT @NAME We agree,. Join our #missiontouranus to help save mankind. Visit https://bit.ly/3p7OoNU"
                     },
                     {
                         id: 118, 
-                        response: "RT We are on a mission to end colon cancer @NAME. Join our #missiontouranus. Explore the unkwonw with a colonoscopy and #fxckcancer. Visit https://bit.ly/3p7OoNU"
+                        response: "RT We are on a mission to end colon cancer @NAME. Join our #missiontouranus. Explore the unkwonw with a colonoscopy and. Visit https://bit.ly/3p7OoNU"
                     },
                     {
                         id: 119, 
-                        response: "RT You had us at #fxckcancer @NAME. Join our #missiontouranus, you can save mankind one colonoscopy at a time. Be a hero. Visit https://bit.ly/3p7OoNU"
-                    }
-                ]
-            }
-        ]
-    },
-    { 
-        id: 114,
-        trigger: '#ColorectalCancerAwarenessMonth',
-        responses: [
-            {
-                start: '2022-01-24 00:00:00',
-                end: '2022-12-31 23:59:59',
-                responses: [
-                    {
-                        id: 118, 
-                        response: "Uranus is the most important mission on Earth. Did you know that 9 in 10 people beat colon cancer when caught early with a colonoscopy? Visit and learn more https://bit.ly/3p7OoNU  #MissionToUranus #fxckcancer #CheckYourColon"
+                        response: "RT You had us at @NAME. Join our #missiontouranus, you can save mankind one colonoscopy at a time. Be a hero. Visit https://bit.ly/3p7OoNU"
                     }
                 ]
             }
