79c5ebdcbef2945baff7b3ff47bc083809eda87f
Raphael Monteiro <raphaelmonteiro@Raphaels-MacBook-Pro-6.local>
2023-01-19 14:38:17 +0000

    create bot message from nature sweet

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,295 +1,59 @@
 module.exports = [
     { 
-        id: 333,
-        trigger: '"Golden Guac" + #GuacKeepers',
+        id: 400,
+        trigger: '#FieldGoalFeast + NatureSweet + idktestsandtests',
         responses: [
             {
                 start: '2022-11-21 00:00:01',
                 end: '2023-12-31 23:59:59',
                 responses: [
                     {
-                        id: 319, 
-                        response: "You are a goodness champion!🏆 Thanks for sharing the rarest card and entering the drawing for a $500 cash prize.💸 Keep it #AlwaysGood by sharing your other cards so your friends can become #GuacKeepers too!🥑 #Sweepstakes #Takis https://bit.ly/FlavorChallenge2022"
+                        id: 401, 
+                        response: "[@user] Thanks for playing along with our #FieldGoalFeast. You're a real MVP! Catch even more friendly finger footballin' fun on naturesweet.com/biggame"
+                    },
+                    {
+                        id: 402, 
+                        response: "[@user] Nice play! Thanks for joining our #FieldGoalFeast. Be sure to catch even more delicious game day fun and see how other friends of the feast performed on naturesweet.com/biggame"
                     }
                 ]
             }
         ]
     },
     { 
-        id: 334,
-        trigger: '"found the #AlwaysGood" + #GuacKeepers',
+        id: 403,
+        trigger: '#sweepstakes + NatureSweet + idktestsandtests',
         responses: [
             {
                 start: '2022-11-21 00:00:01',
                 end: '2023-12-31 23:59:59',
                 responses: [
                     {
-                        id: 320, 
-                        response: "You can really talk the talk and guac the guac! The best #GuacKeepers keep it good by sharing all the cards in their album because guac made with Avocados From Mexico® and #Takis is #AlwaysGood when enjoyed with friends.🥑🔥#Sweepstakes https://bit.ly/3UCYmUf"
+                        id: 404, 
+                        response: "[@user]  Nicely done. How will you be making NatureSweet tomatoes an MVP of your own game day feast? naturesweet.com/biggame"
+                    },
+                    {
+                        id: 405, 
+                        response: "[@user] Nice play! How will you be drafting NatureSweet tomatoes into your own game day feast? naturesweet.com/biggame"
                     }
                 ]
             }
         ]
     },
     { 
-        id: 335,
-        trigger: '"serve #AlwaysGood guac" + #GuacKeepers',
+        id: 406,
+        trigger: '#gameday + NatureSweet + idktestsandtests',
         responses: [
             {
                 start: '2022-11-21 00:00:01',
                 end: '2023-12-31 23:59:59',
                 responses: [
                     {
-                        id: 321, 
-                        response: "Congrats! You’re one of the best #GuacKeepers in history!⚽🥑🥅Gain more entries by sharing your collection of cards and serving #AlwaysGood Avocados From Mexico® and #Takis at your next watch party. 🎉#Sweepstakes https://bit.ly/FlavorChallenge2022"
-                    }
-                ]
-            }
-        ]
-    },
-    { 
-        id: 336,
-        trigger: '"@RafaMarquezMX card" + #GuacKeepers',
-        responses: [
-            {
-                start: '2022-11-21 00:00:01',
-                end: '2023-12-31 23:59:59',
-                responses: [
-                    {
-                        id: 322, 
-                        response: "Way to stay in the game.⚽👏Congrats on scoring the #Takis refuel recipe from @RafaMarquezMX! Keep kickin’ it and earn more entries by adding and sharing your cards to get more #GuacKeepers on the team!🥑#AlwaysGood #Sweepstakes https://bit.ly/FlavorChallenge2022"
-                    }
-                ]
-            }
-        ]
-    },
-    { 
-        id: 337,
-        trigger: '"@landondonovan card" + #GuacKeepers',
-        responses: [
-            {
-                start: '2022-11-21 00:00:01',
-                end: '2023-12-31 23:59:59',
-                responses: [
-                    {
-                        id: 323, 
-                        response: "@landondonovan is loving that you are sharing his #AlwaysGood Loaded Guakis Breakfast Burrito for more #GuacKeepers to enjoy!🥑🌯Share the goodness by adding more Avocados From Mexico® and #Takis recipes to your album.🥰#Sweepstakes https://bit.ly/FlavorChallenge2022"
-                    }
-                ]
-            }
-        ]
-    },
-    { 
-        id: 338,
-        trigger: '"just got the power duo" + #GuacKeepers',
-        responses: [
-            {
-                start: '2022-11-21 00:00:01',
-                end: '2023-12-31 23:59:59',
-                responses: [
-                    {
-                        id: 324, 
-                        response: "Playing with the big leagues now!🥳 Keep sharing the #AlwaysGood power of #GuacKeepers, Avocados From Mexico®, and #Takis of this legendary treble by adding and sharing all of your cards and completing your album.⚽💪#Sweepstakes https://bit.ly/FlavorChallenge2022"
-                    }
-                ]
-            }
-        ]
-    },
-    { 
-        id: 339,
-        trigger: '"Avocados From Mexico® Card" + #GuacKeepers',
-        responses: [
-            {
-                start: '2022-11-21 00:00:01',
-                end: '2023-12-31 23:59:59',
-                responses: [
-                    {
-                        id: 325, 
-                        response: "🥑+⚽=😁 That’s the #AlwaysGood Avocados From Mexico® spirit! Congrats on finding the card that unites all #Guackeepers. Keep collecting cards to complete your album and sharing them for more entries!🏆 #Sweepstakes #Takis https://bit.ly/FlavorChallenge2022"
-                    }
-                ]
-            }
-        ]
-    },
-    { 
-        id: 340,
-        trigger: '"Preserve Guac card" + #GuacKeepers',
-        responses: [
-            {
-                start: '2022-11-21 00:00:01',
-                end: '2023-12-31 23:59:59',
-                responses: [
-                    {
-                        id: 326, 
-                        response: "Kudos on keeping your guac #AlwaysGood 👏You’re keeping the goodness going with guac that lasts all game long. 🎉Complete your album and share more of your #GuacKeepers knowledge for more chances to win. #Takis #Sweepstakes https://bit.ly/FlavorChallenge2022"
-                    }
-                ]
-            }
-        ]
-    },
-    { 
-        id: 341,
-        trigger: '"Guacamole for a Crowd card" + #GuacKeepers',
-        responses: [
-            {
-                start: '2022-11-21 00:00:01',
-                end: '2023-12-31 23:59:59',
-                responses: [
-                    {
-                        id: 327, 
-                        response: "Let’s go! More is better, especially when it comes to #AlwaysGood guacamole.🥑 Keep the party going and make sure your guests are good by sharing as many cards as you can before the final whistle.🥳#Takis #GuacKeepers #Sweepstakes https://bit.ly/FlavorChallenge2022"
-                    }
-                ]
-            }
-        ]
-    },
-    { 
-        id: 342,
-        trigger: '"Good Fats card" + #GuacKeepers',
-        responses: [
-            {
-                start: '2022-11-21 00:00:01',
-                end: '2023-12-31 23:59:59',
-                responses: [
-                    {
-                        id: 328, 
-                        response: "Thanks for sharing the #AlwaysGood fats facts! Avocados From Mexico® has your back when it comes to super watch party snacking.🥑 With nutrient boosting properties, avocados are your team’s MVP.🥇#GuacKeepers #Takis #Sweepstakes https://bit.ly/FlavorChallenge2022"
-                    }
-                ]
-            }
-        ]
-    },
-    { 
-        id: 343,
-        trigger: '"Vitamins & Minerals card" + #GuacKeepers',
-        responses: [
-            {
-                start: '2022-11-21 00:00:01',
-                end: '2023-12-31 23:59:59',
-                responses: [
-                    {
-                        id: 329, 
-                        response: "Who knew avocados contained #AlwaysGood fiber, minerals, and more? Does now! Keep the goodness going by completing your album and sharing extra avo fun facts with other #GuacKeepers.🥑 #Takis #Sweepstakes https://bit.ly/FlavorChallenge2022"
+                        id: 407, 
+                        response: "[@user] Nice play! How will you be drafting NatureSweet tomatoes into your own game day feast? naturesweet.com/biggame"
                     },
                     {
-                        id: 330, 
-                        response: "Did you know that a 50g serving of #AlwaysGood Avocados From Mexico® contributes nearly 20 vitamins, minerals, and other nutrients? Does now! Keep playing and share all your cards with other #GuacKeepers.🥑 #Takis #Sweepstakes https://bit.ly/FlavorChallenge2022"
-                    }
-                ]
-            }
-        ]
-    },
-    { 
-        id: 345,
-        trigger: '"#Takis Logo card" + #GuacKeepers',
-        responses: [
-            {
-                start: '2022-11-21 00:00:01',
-                end: '2023-12-31 23:59:59',
-                responses: [
-                    {
-                        id: 331, 
-                        response: "Takes the lead with #Takis! Scoring this card is a crucial goal of the game.🥵Ramp up the #AlwaysGood intensity and earn more entries by sharing flaming hot @TakisUSA recipe tips and other flavors with fellow #GuacKeepers!🥑🔥 #Sweepstakes https://bit.ly/3UCYmUf"
-                    },
-                    {
-                        id: 332, 
-                        response: "You’ve found one of the five famously fierce @TakisUSA cards!🔥Complete your #AlwaysGood #Takis collection and share the intensity with friends and family on #Soccer game day and every day.🥑Keep it up! #GuacKeepers #Sweepstakes https://bit.ly/FlavorChallenge2022"
-                    },
-                    {
-                        id: 333, 
-                        response: "Congrats on finding one of five tasty #Takis cards!😋 Only extreme @TakisUSA #GuacKeepers will find them all — are you up for the #Soccer challenge? Run, kick, and don’t trip as you score the next #AlwaysGood card ⚽🥑🔥#Sweepstakes https://bit.ly/FlavorChallenge2022"
-                    },
-                    {
-                        id: 334, 
-                        response: "Brings the heat!🔥Keep your match day #AlwaysGood and collect and share the remaining four flamin’ #Takis cards to help complete your album, ramp up the intensity, and get the crowd going!⚽🎉#GuacKeepers #Sweepstakes https://bit.ly/FlavorChallenge2022"
-                    },
-                    {
-                        id: 335, 
-                        response: "Low on snacks? Has you covered! #GuacKeepers stay satisfied during match watch parties with #AlwaysGood @TakisUSA snacks.😋 Collect and share the extreme goodness of #Takis for more chances to win prizes! #Sweepstakes https://bit.ly/FlavorChallenge2022"
-                    }
-                ]
-            }
-        ]
-    },
-    { 
-        id: 346,
-        trigger: '"Fuego® card" + #GuacKeepers',
-        responses: [
-            {
-                start: '2022-11-21 00:00:01',
-                end: '2023-12-31 23:59:59',
-                responses: [
-                    {
-                        id: 336, 
-                        response: "Takes the lead with #Takis! Scoring this card is a crucial goal of the game.🥵Ramp up the #AlwaysGood intensity and earn more entries by sharing flaming hot @TakisUSA recipe tips and other flavors with fellow #GuacKeepers!🥑🔥 #Sweepstakes https://bit.ly/3UCYmUf"
-                    },
-                    {
-                        id: 337, 
-                        response: "You’ve found one of the five famously fierce @TakisUSA cards!🔥Complete your #AlwaysGood #Takis collection and share the intensity with friends and family on #Soccer game day and every day.🥑Keep it up! #GuacKeepers #Sweepstakes https://bit.ly/FlavorChallenge2022"
-                    },
-                    {
-                        id: 338, 
-                        response: "Congrats on finding one of five tasty #Takis cards!😋 Only extreme @TakisUSA #GuacKeepers will find them all — are you up for the #Soccer challenge? Run, kick, and don’t trip as you score the next #AlwaysGood card ⚽🥑🔥#Sweepstakes https://bit.ly/FlavorChallenge2022"
-                    },
-                    {
-                        id: 339, 
-                        response: "Brings the heat!🔥Keep your match day #AlwaysGood and collect and share the remaining four flamin’ #Takis cards to help complete your album, ramp up the intensity, and get the crowd going!⚽🎉#GuacKeepers #Sweepstakes https://bit.ly/FlavorChallenge2022"
-                    },
-                    {
-                        id: 340, 
-                        response: "Low on snacks? Has you covered! #GuacKeepers stay satisfied during match watch parties with #AlwaysGood @TakisUSA snacks.😋 Collect and share the extreme goodness of #Takis for more chances to win prizes! #Sweepstakes https://bit.ly/FlavorChallenge2022"
-                    }
-                ]
-            }
-        ]
-    },
-    { 
-        id: 347,
-        trigger: '"#Takis trifecta" + #GuacKeepers',
-        responses: [
-            {
-                start: '2022-11-21 00:00:01',
-                end: '2023-12-31 23:59:59',
-                responses: [
-                    {
-                        id: 341, 
-                        response: "Takes the lead with #Takis! Scoring this card is a crucial goal of the game.🥵Ramp up the #AlwaysGood intensity and earn more entries by sharing flaming hot @TakisUSA recipe tips and other flavors with fellow #GuacKeepers!🥑🔥 #Sweepstakes https://bit.ly/3UCYmUf"
-                    },
-                    {
-                        id: 342, 
-                        response: "You’ve found one of the five famously fierce @TakisUSA cards!🔥Complete your #AlwaysGood #Takis collection and share the intensity with friends and family on #Soccer game day and every day.🥑Keep it up! #GuacKeepers #Sweepstakes https://bit.ly/FlavorChallenge2022"
-                    },
-                    {
-                        id: 343, 
-                        response: "Congrats on finding one of five tasty #Takis cards!😋 Only extreme @TakisUSA #GuacKeepers will find them all — are you up for the #Soccer challenge? Run, kick, and don’t trip as you score the next #AlwaysGood card ⚽🥑🔥#Sweepstakes https://bit.ly/FlavorChallenge2022"
-                    },
-                    {
-                        id: 344, 
-                        response: "Brings the heat!🔥Keep your match day #AlwaysGood and collect and share the remaining four flamin’ #Takis cards to help complete your album, ramp up the intensity, and get the crowd going!⚽🎉#GuacKeepers #Sweepstakes https://bit.ly/FlavorChallenge2022"
-                    },
-                    {
-                        id: 345, 
-                        response: "Low on snacks? Has you covered! #GuacKeepers stay satisfied during match watch parties with #AlwaysGood @TakisUSA snacks.😋 Collect and share the extreme goodness of #Takis for more chances to win prizes! #Sweepstakes https://bit.ly/FlavorChallenge2022"
-                    }
-                ]
-            }
-        ]
-    },
-    { 
-        id: 348,
-        trigger: '"fuel up on #AlwaysGood" + #GuacKeepers',
-        responses: [
-            {
-                start: '2022-11-21 00:00:01',
-                end: '2023-12-31 23:59:59',
-                responses: [
-                    {
-                        id: 346, 
-                        response: "Congrats on adding an #AlwaysGood match day recipe to your album. 🥑🔥Keep collecting delicious dishes from #Takis and Avocados From Mexico® to share with your fellow #GuacKeepers😋 for more chances to win! #Sweepstakes https://bit.ly/FlavorChallenge2022"
-                    },
-                    {
-                        id: 347, 
-                        response: "#AlwaysGood Avocados From Mexico® and #Takis🤝 are #Soccer match day fire. Congrats on scoring this elite combo! Share more recipes for more chances to win and to enjoy this tasty alliance with friends!⚽🥑 #GuacKeepers #Sweepstakes https://bit.ly/FlavorChallenge2022"
+                        id: 408, 
+                        response: "[@user]  Nicely done. How will you be making NatureSweet tomatoes an MVP of your own game day feast? naturesweet.com/biggame"
                     }
                 ]
             }
